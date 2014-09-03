219683a6fa815fcf05e7cdb6f69863c031200944
Laurent Dami <dami@cpan.org>
2014-09-03 09:30:59 +0200

    merge from List-Categorize-Multi

--- lib/List/Categorize.pm
+++ lib/List/Categorize.pm
@@ -1,18 +1,20 @@
 package List::Categorize;
 #
-# ABSTRACT: Categorize list items into a hash of named sublists.
+# ABSTRACT: Categorize list items into a tree of named sublists.
 #
 # See documentation after __END__ below.
 # 
 
 use strict;
 use warnings;
+use 5.006;
 
 ## Module Interface
 
 use base 'Exporter';
+use Carp qw/croak/;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 our @EXPORT = qw();
 our @EXPORT_OK = qw(
     categorize
@@ -30,13 +32,17 @@ sub categorize (&@)
 # Returns:  a hash of lists
 #
 # Creates a hash by running a subroutine for each element in a list. The
-# subroutine returns a hash key (the "category") for the current
+# subroutine returns a list of hash keys (the "categories") for the current
 # element. (If the subroutine returns undef for a list element, that
 # element is not placed in the resulting hash.)
 #
-# The resulting hash contains a key for each category, and each key
-# refers to a list of the elements that correspond to that category.
-# 
+# The resulting hash contains a key for each first-level category, and
+# each key refers to a sub-hash for the next-level category,
+# etc. until reaching a leaf, which contains a list of the elements
+# that correspond to that sequence of categories. If there is only one
+# level of categories, the structure is just a hashref of lists of
+# elements.
+#
 {
     # Parameters
     #
@@ -44,9 +50,9 @@ sub categorize (&@)
 
     # @_ is used directly, in the loop below.
 
-    # This is the hash of lists that will be returned to the caller.
+    # This is the tree that will be returned to the caller.
     #
-    my %sublists = ();
+    my %tree = ();
 
     # Iterate over the provided list, categorizing
     # each element.
@@ -63,25 +69,44 @@ sub categorize (&@)
         # 
         local $_ = $element;
 
-        # Execute the categorizer subroutine to determine the category
+        # Execute the categorizer subroutine to determine the categories
         # for this element.
         #
-        my $category = $coderef->();
+        my @categories = $coderef->();
 
-        # If a category was returned, use it as a key in the %sublists
+        # If categories were returned, use them as keys in the %tree
         # hash, and add the current element to the list referenced by
-        # that key.
+        # those keys.
         #
         # If the categorizer didn't return a value (or returned undef),
-        # then leave this element out of %sublists entirely.
+        # then leave this element out of %tree entirely.
         #
-        if (defined $category)
-        {
-            push @{ $sublists{ $category } }, $_;
+
+        my $node = \%tree;
+      CATEGORY:
+        while (@categories) {
+            my $categ = shift @categories;
+
+            # if an undef is encountered, this element will be ignored
+            defined $categ or last CATEGORY;
+
+            if (@categories) {
+                # other keys remaining, so create or retrieve an intermediate node
+                $node = $node->{$categ} ||= {};
+                ref $node ne 'ARRAY'
+                  or croak "inconsistent use of category '$categ'";
+            }
+            else {
+                # add the element to a leaf
+                my $ref = ref $node->{$categ} || '';
+                $ref ne 'HASH'
+                  or croak "inconsistent use of category '$categ'";
+                push @{$node->{$categ}}, $_;
+            }
         }
     }
 
-    return %sublists;
+    return %tree;
 }
 
 
@@ -91,11 +116,11 @@ __END__
 
 =head1 NAME
 
-List::Categorize - Categorize list items into a hash of named sublists
+List::Categorize - Categorize list items into a tree of named sublists
 
 =head1 VERSION
 
-This documentation describes List::Categorize version 0.01.
+This documentation describes List::Categorize version 0.04.
 
 =head1 SYNOPSIS
 
@@ -111,21 +136,22 @@ This documentation describes List::Categorize version 0.01.
         # Transform the element before placing it in the hash.
         $_ = ucfirst $_;
 
-        # Use the first letter of the element as the category.
-        substr($_, 0, 1);
+        # Use the first letter of the element as the first-level category,
+        # then the first 2 letters as a second-level category
+        substr($_, 0, 1), substr($_, 0, 2);
 
-    } qw( apple banana antelope bear canteloupe coyote );
+    } qw( apple banana antelope bear canteloupe coyote ananas );
 
     # %capitalized now contains
     # (
-    #   A => [ 'Apple', 'Antelope' ],
-    #   B => [ 'Banana', 'Bear' ],
-    #   C => [ 'Canteloupe', 'Coyote' ]
+    #   A => { An => ['Antelope', 'Ananas'], Ap => ['Apple'], },
+    #   B => { Ba => ['Banana'],             Be => ['Bear'],  },
+    #   C => { Ca => ['Canteloupe'],         Co => ['Coyote'] },
     # )
 
 =head1 DESCRIPTION
 
-A simple module that creates a hash by applying a specified rule to
+A simple module that creates a tree by applying a specified rule to
 each element of a provided list.
 
 =head1 EXPORT
@@ -138,13 +164,17 @@ Nothing by default.
 
     my %hash = categorize { $_ > 10 ? 'Big' : 'Little' } @list;
 
-C<categorize> creates a hash by running BLOCK for each element in LIST.
-The block returns a hash key (the "category") for the current
-element. (If it returns C<undef> for a list element, that element is
-not placed in the resulting hash.)
+C<categorize> creates a tree by running BLOCK for each element in LIST.
+The block should return a list of "categories" for the current
+element, i.e a list of scalar values corresponding to the sequence
+of subtrees under which this element will be placed.
+If the block returns an empty list,
+or a list containing an C<undef>,
+the corresponding element is not placed in the resulting tree.
 
-The resulting hash contains a key for each category, and each key refers to a
-list of the elements that correspond to that category.
+The resulting tree contains a key for each top-level category.
+Values are either references to subtrees, or references
+to arrayrefs of elements (depending on the depth of the categorization).
 
 Within the block, $_ refers to the current list element. Elements can be
 modified before they're placed in the target hash by modifying the $_
@@ -154,7 +184,7 @@ variable:
 
     # %hash now contains ( List => [ 'ONE', 'TWO', 'THREE' ] )
 
-NOTE: The categorizer should return a string, or C<undef>. Other values
+NOTE: The categorizer should return a list of strings, or C<undef>. Other values
 are reserved for future use, and may cause unpredictable results in the
 current version.
 
@@ -164,7 +194,9 @@ L<List::Part>
 
 =head1 AUTHOR
 
-Bill Odom, C<< <wnodom at cpan.org> >>
+Laurent Dami, C<< <dami at cpan.org> >> (added the multi-level categorization)
+Bill Odom, C<< <wnodom at cpan.org> >> (original author)
+
 
 =head1 BUGS
 
@@ -184,27 +216,32 @@ You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker
+=item RT: CPAN's request tracker
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=List-Categorize>
 
-=item * AnnoCPAN: Annotated CPAN documentation
+=item AnnoCPAN: Annotated CPAN documentation
 
 L<http://annocpan.org/dist/List-Categorize>
 
-=item * CPAN Ratings
+=item CPAN Ratings
 
 L<http://cpanratings.perl.org/d/List-Categorize>
 
-=item * Search CPAN
+=item Search MetaCPAN
 
-L<http://search.cpan.org/dist/List-Categorize/>
+L<https://metacpan.org/module/List::Categorize>
+
+=item Github repository
+
+L<https://github.com/damil/List-Categorize>
+
 
 =back
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright (c) 2009 Bill Odom.
+Copyright (c) 2009 Bill Odom, 2014 Laurent Dami.
 
 This module is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
